01da0e7b5c578755dc9857b99d4cbaca143a75a6
gari950 <80147820+gari950@users.noreply.github.com>
2022-06-30 19:20:22 +0530

    Time: 49 ms (19.78%), Space: 16.3 MB (92.36%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,35 +1,64 @@
 class MinStack {
 public:
-    stack<int> s;
-    stack<int> ss;
+    stack<long long int> s;
+    long long int minval = -1;
     MinStack() {
         
     }
     
-    void push(int val) {
-        s.push(val);
-       if(ss.size()==0 || val<=ss.top())
-           ss.push(val);
-        
+    void push(long long int val) {
+        if(s.size()==0)
+        {
+            s.push(val);
+            minval = val;
+        }
+        else
+            if(val >=minval)
+                s.push(val);
+        else
+            if(val < minval)
+            {
+                s.push(2*val - minval);
+                minval = val;
+            }
     }
     
     void pop() {
-        if(s.size()==0)  
-          cout<<-1;
-        int ans = s.top();
-        s.pop();
-        if(ss.top()==ans)
-            ss.pop();
+        if(s.size()==0)
+        {
+            minval = -1;
+            return;
+        }
+        else
+            if(s.top() >= minval)
+                s.pop();
+        else
+            if(s.top() < minval)
+            {
+                minval= 2*minval - s.top();
+                s.pop();
+            }
     }
     
     int top() {
-        return s.top();
+        if(s.size()==0)
+            return -1;
+        else
+            if(s.top() >= minval)
+                return s.top();
+        else
+            if(s.top() < minval)
+            {
+                return minval;
+            }
+        return -1;
     }
     
     int getMin() {
-      if(ss.size()==0)  
-          return -1;
-        return ss.top();
+        if(s.size()==0)
+            return -1;
+        else
+            return minval;
     }
 };
 
