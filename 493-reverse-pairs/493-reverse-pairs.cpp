d912d4d3569509a6f5a860d244f6f4b6eec1fa0a
boomer_19 <80147820+gari950@users.noreply.github.com>
2022-07-06 23:56:46 +0530

    Time: 684 ms (59.48%), Space: 44.9 MB (99.01%) - LeetHub

---
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        int l = 0, r = nums.size()-1 ,c = 0;
+        mergesort(nums , l , r, c);
+        return c;
+    }
+    
+    void mergesort(vector<int>& nums , int left, int right, int& c)
+    {
+        if(left<right)
+        {
+           int mid = (right + left)/2;
+            mergesort(nums, left , mid, c);
+            mergesort(nums, mid+1, right, c);
+            merge(nums,left,mid,right,c);
+        }
+    }
+    
+    void merge(vector<int>& nums, int left, int mid,int right, int& c)
+    {
+        int i = left, j = mid+1;
+        while(i<=mid && j<=right)
+        {
+            if(nums[i] > 2*1ll*nums[j])
+            {
+                c+=(mid-i+1);
+                j++;
+            }
+            else
+                i++;
+        }
+        
+        sort(nums.begin()+left,nums.begin()+right+1);
+        
+    }
+};
