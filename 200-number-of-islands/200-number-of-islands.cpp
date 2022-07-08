dc40ad7f979a58efc2c5aff750acd675d8414e3e
boomer_19 <80147820+gari950@users.noreply.github.com>
2022-07-08 16:26:53 +0530

    Time: 53 ms (51.64%), Space: 18.3 MB (12.04%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,37 +1,52 @@
 class Solution {
 public:
+    
+    bool inside(int new_row , int new_col, vector<vector<char>>& grid)
+        {
+            return 0 <= new_row  && new_row < grid.size() && 0 <= new_col && new_col < grid[0].size();
+
+        }
+    
     int numIslands(vector<vector<char>>& grid) {
+       if( grid.empty() || grid[0].empty())
+           return 0;
+        
         int m = grid.size();
         int n = grid[0].size();
         int c=0;
-        vector<vector<bool>> visited( m , vector<bool> (n));
+        
+        vector<pair<int,int>> direction = {{1,0},{0,1},{-1,0},{0,-1}};
+        vector<vector<bool>> vis( m , vector<bool> (n));
         for(int i=0;i<m;i++ )
         {
             for(int j=0;j<n;j++ )
             {
-            if(grid[i][j] =='1' && visited[i][j]==false)
+            if(grid[i][j] =='1' && !vis[i][j])
             {
-                dfs(grid,i,j,visited);
                 c++;
-            }
+                queue<pair<int,int>> q;
+                q.push({i,j});
+                vis[i][j]=true;
+                
+                while(!q.empty())
+                {
+                    pair<int,int> p = q.front();
+                    q.pop();
+                    for(pair<int,int> dir : direction)
+                    {
+                        int new_row = p.first + dir.first;
+                        int new_col = p.second + dir.second;
+                        if(inside(new_row,new_col,grid) && !vis[new_row][new_col] && grid[new_row][new_col]=='1')
+                        {
+                            q.push({new_row,new_col});
+                            vis[new_row][new_col]=true;
+                        }
+                    }
+                }
             }
         }
-        return c;
-    }
-    
-    void dfs(vector<vector<char>>& grid,int i,int j, vector<vector<bool>>& visited)
-    {
-        int m = grid.size();
-        int n = grid[0].size();
-        if(i<0 || j<0 || i>=m || j>=n || visited[i][j]==true || grid[i][j]=='0')
-            return;
+     }
+     return c;   
         
-         visited[i][j]=true;
-         dfs(grid,i-1,j,visited);//north
-         dfs(grid,i+1,j,visited);//south
-         dfs(grid,i,j-1,visited);//west
-         dfs(grid,i ,j+1,visited);//east
-       
-    
     }
 };
