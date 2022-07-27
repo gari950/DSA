24b88ff2eef1755c5df6a2f8d533557228554702
boomer_19 <80147820+gari950@users.noreply.github.com>
2022-07-28 00:34:13 +0530

    Time: 0 ms (100.00%), Space: 7 MB (58.29%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -8,7 +8,7 @@ public:
     
     void gameOfLife(vector<vector<int>>& board) {
        int m = board.size(), n = board[0].size();    
-        vector<vector<int>> res(m , vector<int> (n, 0));
+       // vector<vector<int>> res(m , vector<int> (n, 0));
        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
@@ -17,26 +17,31 @@ public:
                     int p = i + d.first;
                     int q = j + d.second;
                 
-                if(check(p,q,board) && board[p][q]==1)
+                if(check(p,q,board) && (board[p][q]==1 || board[p][q]==2))
                     count++;
             }
-        
+        // 2 = alive -> dead
         if(board[i][j]==1){
             
-            if(count == 2 || count == 3){
-                res[i][j] = 1;
+            if(count < 2 || count > 3){  //Over or Under population
+                //res[i][j] = 1;
+                board[i][j]=2;
             }
         }
             else{
+                // 3 = dead -> alive
                 if(count == 3){
-                    res[i][j] = 1;
+                   // res[i][j] = 1;
+                    board[i][j]=3;
                 }
             }
        }
     }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                board[i][j] = res[i][j];
+                //board[i][j] = res[i][j];
+                board[i][j]%=2;  // 1%2 == 1 || 3%2 == 1 -> alive
+                                // 2%2 == 0 -> dead
             }
         }
     }
