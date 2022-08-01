9eab10530468687861598c94569deaf1a446c2b8
boomer_19 <80147820+gari950@users.noreply.github.com>
2022-08-01 23:46:03 +0530

    Time: 8 ms (59.55%), Space: 16.3 MB (93.85%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -12,21 +12,17 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-         if (!root) return true;
         return helper(root->left, root->right);
     }
     
     bool helper(TreeNode* p, TreeNode* q) {
         if (!p && !q) {
             return true;
-        } else if (!p || !q) {
+        } 
+        if (!p || !q) {
             return false;
         }
         
-        if (p->val != q->val) {
-            return false;
-        }
-        
-        return helper(p->left,q->right) && helper(p->right, q->left); 
+        return p->val==q->val && helper(p->left,q->right) && helper(p->right, q->left); 
     }
 };
