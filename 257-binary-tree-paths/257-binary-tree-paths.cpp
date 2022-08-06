f7002b9518d825bc3aa2d134838e4163f06929be
boomer_19 <80147820+gari950@users.noreply.github.com>
2022-08-06 14:31:19 +0530

    Time: 0 ms (100.00%), Space: 13.1 MB (64.56%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -14,21 +14,21 @@ public:
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string> res;
         if(!root)return res;
-        helper(res,root,to_string(root->val));
+        binaryTreePaths(res,root,to_string(root->val));
         return res;
     }
     
-    void helper(vector<string>& res, TreeNode* root, string t){
+    void binaryTreePaths(vector<string>& res, TreeNode* root, string t){
         if(!root->left && !root->right){
             res.push_back(t);
             return;
         }
         
         if(root->left){
-           helper(res, root->left, t + "->" + to_string(root->left->val)) ;
+           binaryTreePaths(res, root->left, t + "->" + to_string(root->left->val)) ;
         }
         if(root->right){
-          helper(res, root->right, t + "->" + to_string(root->right->val)) ;  
+          binaryTreePaths(res, root->right, t + "->" + to_string(root->right->val)) ;  
         }
     }
 };
