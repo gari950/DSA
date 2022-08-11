80d629d0a83954c8513572a6c1b88ba62b7f7324
boomer_19 <80147820+gari950@users.noreply.github.com>
2022-08-11 23:08:51 +0530

    Time: 46 ms (10.93%), Space: 25.2 MB (51.25%) - LeetHub

--- 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.cpp
+++ 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.cpp
@@ -11,15 +11,17 @@
  */
 class Solution {
 public:
-   int min_dif = INT_MAX, val = -1;    
-int getMinimumDifference(TreeNode* root) {
-    if (root->left != NULL) 
-        getMinimumDifference(root->left);
-    if (val >= 0) 
-        min_dif = min(min_dif, root->val - val);
-    val = root->val;
-    if (root->right != NULL) 
-        getMinimumDifference(root->right);
-    return min_dif;
-}
+    int diff= INT_MAX;
+    TreeNode *prev = NULL;
+    int getMinimumDifference(TreeNode* root) {
+        if(root->left)
+            getMinimumDifference(root->left);
+        if(prev){
+            diff = min(diff,abs(root->val-prev->val));
+        }
+        prev = root;
+        if(root->right)
+            getMinimumDifference(root->right);
+        return diff;
+    }
 };
