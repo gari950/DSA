e7b3dba90c5cd0204c6bc547d6028863194ca6bd
boomer_19 <80147820+gari950@users.noreply.github.com>
2022-08-25 22:39:13 +0530

    Time: 72 ms (37.96%), Space: 28.3 MB (83.88%) - LeetHub

---
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int m = INT_MAX;
+        for(auto n : nums)
+            m = min(m,n);
+        int ans=0;
+        for(auto n: nums)
+            ans+=n-m;
+        return ans;
+    }
+};
