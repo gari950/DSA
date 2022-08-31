4f59064fbaffbc3fd9a66c19b4966c23b48023c2
boomer_19 <80147820+gari950@users.noreply.github.com>
2022-08-31 23:33:14 +0530

    Time: 39 ms (6.51%), Space: 13.3 MB (53.42%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        set<int> row;
-        set<int> col;
-        
-        for(int i=0;i<matrix.size();i++)
+    void dfs(int i,int j, int m, int n,vector<vector<int>>& mat,vector<vector<bool>>& vis){
+        vis[i][j]= true;
+        for(int row=0;row<m;row++)
         {
-            for(int j=0;j<matrix[0].size();j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    row.insert(i);
-                    col.insert(j);
-                }
+            if(mat[row][j]!=0){
+                mat[row][j]=0;
+                vis[row][j]=true;
             }
         }
-        
-        for(int i=0;i<matrix.size();i++)
-        {
-            for(int j=0;j<matrix[0].size();j++)
-            {
-                if(row.count(i) || col.count(j))
-                {
-                    matrix[i][j]=0;
-                }
+        for(int col=0;col<n;col++){
+            if(mat[i][col]!=0){
+                mat[i][col]=0;
+                vis[i][col]=true;
             }
         }
-        
+    }
+    
+    void setZeroes(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<vector<bool>> vis(m,vector<bool>(n,false));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0 && vis[i][j]==false)
+                    dfs(i,j,m,n,mat,vis);
+            }
+        }
     }
 };
