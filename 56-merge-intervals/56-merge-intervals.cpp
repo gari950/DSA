0cbefa55cf2912dae93bcd055f44a90b0979e69f
boomer_19 <80147820+gari950@users.noreply.github.com>
2022-09-06 23:09:49 +0530

    Time: 32 ms (99.02%), Space: 19.1 MB (67.45%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& arr) {
-        vector<vector<int>> ans;
-        if(arr.size()==0)
-            return ans;
-        sort(arr.begin(),arr.end());
-        vector<int> temp = arr[0];
-        for(auto it : arr){
-            if(it[0] <= temp[1]){
-                 //cout<<temp[1]<<" "<<it[0]<<endl;
-                temp[1] = max(it[1],temp[1]);
-               //cout<<temp[1]<<endl;
-                }
-                else{
-                 ans.push_back(temp);
-                    temp = it;
-                }
-        }
-        ans.push_back(temp);
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
         
-        return ans;
+        vector<vector<int>> list;
+        if(intervals.size()<=1) return intervals;
+        sort(intervals.begin(),intervals.end());
+        list.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+          if(intervals[i][0] <= list.back()[1])
+              list.back()[1] = max(intervals[i][1],list.back()[1]);
+            else 
+                list.push_back(intervals[i]);
+        }
+          return list;  
     }
 };
