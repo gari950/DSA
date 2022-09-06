5e6d7e3fa406cdeebf7355424c560ec56a0461b6
boomer_19 <80147820+gari950@users.noreply.github.com>
2022-09-07 00:26:42 +0530

    Time: 4 ms (54.91%), Space: 9 MB (72.59%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int>c;
-        int i=0,j=0;
+        vector<int> nums;
+        int i=0,j=0;        
         while(i+j<m+n){
-            if(i==m){
-                c.push_back(nums2[j]);
+           if(i==m)
+            {
+                nums.push_back(nums2[j]);
                 j++;
             }
-           else
-               if(j==n){
-                c.push_back(nums1[i]);
+            else if(j==n)
+            {
+                nums.push_back(nums1[i]);
                 i++;
             }
-            else
-                if(nums1[i]<=nums2[j]){
-                    c.push_back(nums1[i]);
-                    i++;
-                }
-            else
-                if(nums1[i]>nums2[j]){
-                    c.push_back(nums2[j]);
-                    j++;
-                }
+            else 
+                if(nums1[i] <= nums2[j]){
+                nums.push_back(nums1[i]);
+                i++;
+            }
             
+            else if(nums1[i] > nums2[j]){
+                nums.push_back(nums2[j]);
+                j++;
+            }
         }
-        for(int k=0;k<c.size();k++){
-            swap(nums1[k],c[k]);
-        }
+        
+        for(int k=0;k<nums.size();k++)
+            swap(nums1[k],nums[k]);
     }
 };
